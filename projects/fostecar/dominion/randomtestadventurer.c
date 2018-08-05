bef1b6609f919a4511f24c45df87b8a4c5bb39a9
Carissa Foster <fostecar@flip2.engr.oregonstate.edu>
2018-08-04 19:23:57 -0700

    rand adven test update5

--- projects/fostecar/dominion/randomtestadventurer.c
+++ projects/fostecar/dominion/randomtestadventurer.c
@@ -53,55 +53,55 @@ int gameStateRandomSetup(struct gameState *testGS, int *deckTreasure1, int *deck
 
 	//game player setup
 	(*testGS).numPlayers = rand() % (MAX_PLAYERS + 1 - 1) + 1; //1-4
-	testGS.whoseTurn = rand() % testGS.numPlayers;
+	(*testGS).whoseTurn = rand() % (*testGS).numPlayers;
 
 	//hand setup
-	testGS.handCount[testGS.whoseTurn] = rand() % (MAX_HAND + 1 - 1) + 1; //1 to Max_Hand
+	(*testGS).handCount[(*testGS).whoseTurn] = rand() % (MAX_HAND + 1 - 1) + 1; //1 to Max_Hand
 
 	for (i = 0; i < MAX_HAND; ++i) {
-		if (i < testGS.handCount[testGS.whoseTurn]) {
-			testGS.hand[testGS.whoseTurn][i] = rand() % 27;
+		if (i < (*testGS).handCount[(*testGS).whoseTurn]) {
+			(*testGS).hand[(*testGS).whoseTurn][i] = rand() % 27;
 		}
 		else {
-			testGS.hand[testGS.whoseTurn][i] = -1;
+			(*testGS).hand[(*testGS).whoseTurn][i] = -1;
 		}
 	}
 
 	//adventurer card insert
-	i = rand() % testGS.handCount[testGS.whoseTurn];
-	testGS.hand[testGS.whoseTurn][i] = adventurer;
+	i = rand() % (*testGS).handCount[(*testGS).whoseTurn];
+	(*testGS).hand[(*testGS).whoseTurn][i] = adventurer;
 
 	//deck setup
 	*deckTreasure1 = -1;
 	*deckTreasure2 = -1;
-	testGS.deckCount[testGS.whoseTurn] = rand() % (MAX_DECK + 1 - 1) + 1; //1 to Max_Deck
+	(*testGS).deckCount[(*testGS).whoseTurn] = rand() % (MAX_DECK + 1 - 1) + 1; //1 to Max_Deck
 
 	for (i = 0; i < MAX_DECK; ++i) {
-		if (i < testGS.deckCount[testGS.whoseTurn]) {
-			testGS.deck[testGS.whoseTurn][i] = rand() % 27;
-			if ((testGS.deck[testGS.whoseTurn][i] == copper) || (testGS.deck[testGS.whoseTurn][i] == silver) || (testGS.deck[testGS.whoseTurn][i] == gold)) {
+		if (i < (*testGS).deckCount[(*testGS).whoseTurn]) {
+			(*testGS).deck[(*testGS).whoseTurn][i] = rand() % 27;
+			if (((*testGS).deck[(*testGS).whoseTurn][i] == copper) || ((*testGS).deck[(*testGS).whoseTurn][i] == silver) || ((*testGS).deck[(*testGS).whoseTurn][i] == gold)) {
 				*deckTreasure2 = *deckTreasure1;
 				*deckTreasure1 = i;
 			}
 		}
 		else {
-			testGS.deck[testGS.whoseTurn][i] = -1;
+			(*testGS).deck[(*testGS).whoseTurn][i] = -1;
 		}
 	}
 
 	//discard setup
 	*discardTreasureNum = 0;
-	testGS.discardCount[testGS.whoseTurn] = rand() % ((MAX_DECK - testGS.deckCount[testGS.whoseTurn]) + 1 - 1) + 1; //1 to Max_Deck
+	(*testGS).discardCount[(*testGS).whoseTurn] = rand() % ((MAX_DECK - (*testGS).deckCount[(*testGS).whoseTurn]) + 1 - 1) + 1; //1 to Max_Deck
 
 	for (i = 0; i < MAX_DECK; ++i) {
-		if (i < testGS.discardCount[testGS.whoseTurn]) {
-			testGS.discard[testGS.whoseTurn][i] = rand() % 27;
-			if ((testGS.deck[testGS.whoseTurn][i] == copper) || (testGS.deck[testGS.whoseTurn][i] == silver) || (testGS.deck[testGS.whoseTurn][i] == gold)) {
+		if (i < (*testGS).discardCount[(*testGS).whoseTurn]) {
+			(*testGS).discard[(*testGS).whoseTurn][i] = rand() % 27;
+			if (((*testGS).deck[(*testGS).whoseTurn][i] == copper) || ((*testGS).deck[(*testGS).whoseTurn][i] == silver) || ((*testGS).deck[(*testGS).whoseTurn][i] == gold)) {
 				*discardTreasureNum = *discardTreasureNum + 1;
 			}
 		}
 		else {
-			testGS.discard[testGS.whoseTurn][i] = -1;
+			(*testGS).discard[(*testGS).whoseTurn][i] = -1;
 		}
 	}
 
